99f0aad01712d4b1108673a9038954dcd19502bf
agaiw <iwaszkiewicz.aga@gmail.com>
2017-06-10 11:00:54 +0200

    c style concatenate

--- arrays/concatenate.cpp
+++ arrays/concatenate.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 #include <vector>
+#define SIZE1 3
+#define SIZE2 3
 
 int main(int argc, char* argv[]) {
+  //c style
+  int cv1[SIZE1] = {3, 4, 5};
+  int cv2[SIZE2] = {5, 6, 7};
+  int cv1v2[SIZE1 + SIZE2];
+  for (int i = 0; i < SIZE1; ++i) {
+    cv1v2[i] = cv1[i];
+  }
+  for (int i = SIZE1; i < (SIZE1 + SIZE2); ++i) {
+    cv1v2[i] = cv2[i - SIZE1];
+  }
+ 
+  for (int i = 0; i < (SIZE1 + SIZE2); ++i) {
+    std::cout << cv1v2[i] << " ";
+  }
+  std::cout << std::endl;
 
+
+  //c++ style
   std::vector<int> v1 {1, 2, 3};
   std::vector<int> v2 {1, 2, 5};
   std::vector<int> v1v2;
@@ -10,8 +29,8 @@ int main(int argc, char* argv[]) {
   v1v2.insert(v1v2.end(), v1.begin(), v1.end());
   v1v2.insert(v1v2.end(), v2.begin(), v2.end());
 
-  for (auto i: v1v2) {
-    std::cout << i << " ";
+  for (auto element: v1v2) {
+    std::cout << element << " ";
   }
   std::cout << std::endl;
 
